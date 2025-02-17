0814a7836736d8bbabc28c5432588ebd2601e014
Magdalena <97711560+Magdalenaarce@users.noreply.github.com>
2025-02-17 17:36:28 +0000

    prueba

--- src/script.js
+++ src/script.js
@@ -1,7 +1,6 @@
 let apikey = "ec2tac9a4a7ec807298084203cbof638";
 let context = "";
 let prompt = "";
-let apiurl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${key}`;
 
 function displayjoke(response) {
   let joke = document.querySelector("#joke");
@@ -10,9 +9,9 @@ function displayjoke(response) {
 
 function telljoke(event) {
   let instructions = document.querySelector("#text1");
-  let apiurl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${key}`;
   context = "you are a romantic poem expert and love to write short poems.";
   promtt = `write a poem following the user instructions: ${instructions}`;
+  let apiurl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${key}`;
   axios.get(apiurl).then(displayjoke);
 }
 
