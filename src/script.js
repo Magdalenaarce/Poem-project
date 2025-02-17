5ace4de3368a00e1b702a84021074a9e5b776d0a
Magdalena <97711560+Magdalenaarce@users.noreply.github.com>
2025-02-17 20:37:32 +0000

    now is an ai generator

--- src/script.js
+++ src/script.js
@@ -7,8 +7,8 @@ function telljoke(event) {
   let instructions = document.querySelector("#text1");
   let apikey = "ec2tac9a4a7ec807298084203cbof638";
   let context =
-    "you are a romantic poem expert and love to write short poems of maximum 3 lines, without tittle";
-  let prompt = `write a poem following the user instructions: ${instructions}`;
+    "you give the best answers and preciss, you dont write any context and just answer with list of things such as poems, baby names, recipes or any thing that the user ask you";
+  let prompt = `answer following the user instructions: ${instructions}`;
   let apiurl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apikey}`;
   axios.get(apiurl).then(displayjoke);
 }
