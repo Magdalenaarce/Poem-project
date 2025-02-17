8d8f5650d24bee2f90454b9a25f69b405504541e
Magdalena <97711560+Magdalenaarce@users.noreply.github.com>
2025-02-17 16:59:18 +0000

    js basic

---
+++ src/script.js
@@ -0,0 +1,8 @@
+function telljoke() {
+  let joke = document.querySelector("#joke");
+  joke.innerHTML("pruena generando un chiste");
+}
+
+button1 = document.querySelector("#submit");
+
+button1.addEventListener("click", telljoke);
