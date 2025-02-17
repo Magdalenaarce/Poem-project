4f5a163e3b554ec86f238e9368e918ab8e704b11
Magdalena <97711560+Magdalenaarce@users.noreply.github.com>
2025-02-17 20:43:57 +0000

    Update script.js

--- src/script.js
+++ src/script.js
@@ -7,7 +7,7 @@ function telljoke(event) {
   let instructions = document.querySelector("#text1");
   let apikey = "ec2tac9a4a7ec807298084203cbof638";
   let context =
-    "you give the best answers and preciss, you dont write any context and just answer with list of things such as poems, baby names, recipes or any thing that the user ask you";
+    "you are a cultured and very experienced AI generator and respond to the user with correct answers";
   let prompt = `answer following the user instructions: ${instructions}`;
   let apiurl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apikey}`;
   axios.get(apiurl).then(displayjoke);
