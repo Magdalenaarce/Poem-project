f84e17a996333994427034f039c556802c8f087f
Magdalena <97711560+Magdalenaarce@users.noreply.github.com>
2025-02-17 17:42:21 +0000

    arreflos en las instructions

--- src/script.js
+++ src/script.js
@@ -6,7 +6,8 @@ function displayjoke(response) {
 function telljoke(event) {
   let instructions = document.querySelector("#text1");
   let apikey = "ec2tac9a4a7ec807298084203cbof638";
-  let context = "you are a romantic poem expert and love to write short poems.";
+  let context =
+    "you are a romantic poem expert and love to write short poems of maximum 3 lines, without tittle";
   let prompt = `write a poem following the user instructions: ${instructions}`;
   let apiurl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apikey}`;
   axios.get(apiurl).then(displayjoke);
