e9c45d8c57868dfad9c0cfe1ffe078dc0ddddb8d
Magdalena <97711560+Magdalenaarce@users.noreply.github.com>
2025-02-17 17:28:56 +0000

    prueba ai

--- src/script.js
+++ src/script.js
@@ -1,6 +1,18 @@
-function telljoke() {
+let apikey = "ec2tac9a4a7ec807298084203cbof638";
+let context = "";
+let prompt = "";
+let apiurl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${key}`;
+
+function displayjoke(response) {
   let joke = document.querySelector("#joke");
-  joke.innerHTML = "pruena generando un chiste";
+  joke.innerHTML = response;
+}
+
+function telljoke(event) {
+  let instructions = document.querySelector("#text1");
+  context = "you are a romantic poem expert and love to write short poems.";
+  promtt = `write a poem following the user instructions: ${instructions}`;
+  axios.get(apiurl).then(displayjoke);
 }
 
 button1 = document.querySelector("#submit");
